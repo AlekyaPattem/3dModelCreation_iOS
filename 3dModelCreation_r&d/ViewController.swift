bfc661f6b8f616788c78a94f87716188942232f2
Alekya <alekhya@krify.com>
2025-07-09 18:03:11 +0530

    project file added

---
+++ 3dModelCreation_r&d/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  3dModelCreation_r&d
+//
+//  Created by KSMACMINI-019 on 09/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
